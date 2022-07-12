a2842b89339b2de353483b9f4a8a631516fdbc75
Prity <kumari.13@iitj.ac.in>
2022-07-12 17:49:54 +0530

    initial commit

---
+++ mywebsite/main.py
@@ -0,0 +1,18 @@
+def fun():
+    #thid 
+    #hfrbjgn
+    #hfj
+    #def mun():
+        #pass
+        print("somethong")
+
+print("hello world")
+a = input("enter a number\n")
+print("your number ",a)
+MovieList= ["jdhjfjg jfkdnfk",
+"bjdbjfngn",
+"bcbdsjnk lriijgtklh  kllrgk",
+"vnfbjvn klgk  kllrgmnkm",]
+
+for movie in MovieList:
+    print(movie)
